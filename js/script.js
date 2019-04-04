1fc96b8e467322b60a241f48bcfdcda5cf466a15
Ali Chegini <ali.c.engineer@gmail.com>
2019-04-04 14:58:04 +0200

    completed logic for event bubbling

--- js/script.js
+++ js/script.js
@@ -8,42 +8,37 @@ FSJS project 2 - List Pagination
    need to reference and/or manipulate.
 ***/
 
-
-// var button = document.createElement("button")
-// button.innerHTML = "1"
-
 var page = document.getElementsByClassName("page")[0]
-
-
 var students = document.getElementsByClassName("student-item cf")
-
 var numberOfPages = Math.floor(students.length/10) + 1
 
 
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-***/
+// showPage function to hide all of the items in the
+// list except for the ten that should show.
 function showPage(pageNumber) {
-
+  // loop to hide all the students
   for (i = 0; i < students.length; i++) {
     students[i].style.display = "none"
   }
 
+  // for loop to show certain number of students
   for (i = (pageNumber - 1) * 10; i < (pageNumber * 10); i++) {
-    students[i].style.display = "block"
+    // condition to break out of the loop in last page
+    if (i == students.length) {
+      break;
+    } else {
+      students[i].style.display = "block"
+    }
   }
 
 }
 
+// first page is always activated at launch
+showPage(1)
 
 
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
+// appendPageLinks function to generate, append, and add
+// functionality to the pagination buttons.
 function appendPageLinks() {
 
   for (i = 1; i <= numberOfPages; i++) {
@@ -52,15 +47,20 @@ function appendPageLinks() {
     page.appendChild(button)
   }
 
-  // add eventListener for all buttons
-  // call showPage on each buttons
-
-  var button = document.getElementsByID
-
-  button.addEventListener("click", () => {
-    console.log(parseInt(button.innerHTML));
+  // eventListener for all buttons --- Event Bubbling
+  document.addEventListener("click", (event) => {
+    if (event.target.tagName.toLowerCase() == "button") {
+      var page = parseInt(event.target.outerText)
+      // calling showPage with pageNumber
+      showPage(page)
+    }
   })
 
 }
 
 appendPageLinks()
+
+// Todo :
+// improve layout and design
+// get rid of magic numbers 10
+// active link should be colored and mouse over effect
